4da15bc6d7048ba12d78448a006f10128ef5cedc
onlinebaba <onlinebaba@users.noreply.github.com>
2014-06-22 13:10:27 -0700

    Tidy up column names

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(data.table)
 
 # Read all activities
 activitiesDF <- read.table("activity_labels.txt")
-names(activitiesDF) <- c("ActivityId","ActivityName")
+names(activitiesDF) <- c("activityId","activityName")
 
 # Read all feature names
 featuresDF <- read.table("features.txt")
@@ -13,7 +13,7 @@ featuresNames <- as.vector(featuresDF$V2)
 
 # Read Test Subjects and add column name header
 subjectTestData <- read.table("subject_test.txt")
-names(subjectTestData) <- c("Subject")
+names(subjectTestData) <- c("subject")
 
 # Read X Test data and add columm name headers
 xTestData <- read.table("X_test.txt")
@@ -22,67 +22,71 @@ names(xTestData) <- featuresNames
 
 # Ready Activity y test data and add column name header
 yTestData <- read.table("y_test.txt")
-names(yTestData) <- c("ActivityId")
+names(yTestData) <- c("activityId")
 
 # Combine feature, subject and activity data into one test data set
 combinedTestDataFrame <- as.data.frame(append(data.frame(),list(subjectTestData,yTestData,xTestData)))
 
 # Extract required columns (mean and std cols) from test data set and Label column name correctly
-subActCols <- c("Subject","ActivityId")
-meanStdCols <- c(grep("std()",featuresNames),grep("mean()",featuresNames))
-finalSubActTestDataFrame <- subset(combinedTestDataFrame,select = subActCols )
-finalMeanStdTestDataFrame <- subset(combinedTestDataFrame,select = meanStdCols )
+names(combinedTestDataFrame) <- c("subject","activityId", names(xTestData))
+finalSubActTestDataFrame <- subset(combinedTestDataFrame,select = c("subject","activityId") )
+finalMeanStdTestDataFrame <- subset(combinedTestDataFrame,select = c(names(xTestData[c(grep("[-]mean\\(|[-]std\\(\\)",names(xTestData)))])) )
 finalTestDataFrame <- cbind(finalSubActTestDataFrame,finalMeanStdTestDataFrame)
 
 ############################## Read train data ##############################
 # Read Train Subjects and add column name header
 subjectTrainData <- read.table("subject_train.txt")
-subjectTrainData
-names(subjectTrainData) <- c("Subject")
+names(subjectTrainData) <- c("subject")
 
 # Read X Train data and add columm name headers
 xTrainData <- read.table("X_train.txt")
-xTrainData
 # Normalize column names stripping special chars
 names(xTrainData) <- featuresNames
 
 # Ready Activity y Train data and add column name header
 yTrainData <- read.table("y_train.txt")
-yTrainData
-names(yTrainData) <- c("ActivityId")
+names(yTrainData) <- c("activityId")
 
 # Combine feature, subject and activity data into one Train data set
 combinedTrainDataFrame <- as.data.frame(append(data.frame(),list(subjectTrainData,yTrainData,xTrainData)))
 
-############################## Combine test and train data ##############################
-
-# Extract required columns (mean and std cols) from test data set and Label column name correctly
-finalSubActTrainDataFrame <- subset(combinedTrainDataFrame,select = subActCols )
-finalMeanStdTrainDataFrame <- subset(combinedTrainDataFrame,select = meanStdCols )
+# Extract required columns (mean and std cols) from Train data set and Label column name correctly
+names(combinedTrainDataFrame) <- c("subject","activityId", names(xTrainData))
+finalSubActTrainDataFrame <- subset(combinedTrainDataFrame,select = c("subject","activityId") )
+finalMeanStdTrainDataFrame <- subset(combinedTrainDataFrame,select = c(names(xTrainData[c(grep("[-]mean\\(|[-]std\\(\\)",names(xTrainData)))])) )
 finalTrainDataFrame <- cbind(finalSubActTrainDataFrame,finalMeanStdTrainDataFrame)
-finalMergedDataFrame <- rbind(finalTrainDataFrame,finalTestDataFrame)
 
+############################## Combine test and train data ##############################
+finalMergedDataFrame <- rbind(finalTrainDataFrame,finalTestDataFrame)
 # Check 1 for NA
 # sum(is.na(finalMergedDataFrame))
 # Check 2 for NA
 # sapply(finalMergedDataFrame, function(x) sum(is.na(x)))
 
 ################# Average of each variable for each activity and each subject #################
-myTidyDF <- aggregate(.~Subject+ActivityId, FUN=mean, data=finalMergedDataFrame)
-myTidyDF <- merge(activitiesDF,myTidyDF,by.x="ActivityId",by.y="ActivityId")
+myTidyDF <- aggregate(.~subject+activityId, FUN=mean, data=finalMergedDataFrame)
+myTidyDF <- merge(activitiesDF,myTidyDF,by.x="activityId",by.y="activityId")
 
 # A total of 180 rows: 30 subjects x 6 activities
 dim(myTidyDF)
 
-###### Miscellaneous: Clean up the header names #######
-names(myTidyDF) <- gsub("\\.", "_", names(myTidyDF))
-names(myTidyDF) <- gsub("()-", "_", names(myTidyDF))
-names(myTidyDF) <- gsub("___", "_", names(myTidyDF))
-names(myTidyDF) <- gsub("__", "_", names(myTidyDF))
-names(myTidyDF) <- gsub("fBody", "Body", names(myTidyDF))
-names(myTidyDF) <- gsub("tBody", "Body", names(myTidyDF))
-names(myTidyDF) <- gsub("tGravity", "Gravity", names(myTidyDF))
-names(myTidyDF) <- gsub("BodyBody", "Body", names(myTidyDF))
+################# Clean up column names #################
+names(myTidyDF) <- gsub("[-]mean\\(\\)", "Mean", names(myTidyDF))
+names(myTidyDF) <- gsub("[-]std\\(\\)", "StandardDeviation", names(myTidyDF))
+names(myTidyDF) <- gsub("tBodyAcc", "bodyAcclTime", names(myTidyDF))
+names(myTidyDF) <- gsub("tBodyGyro", "bodyGyroTime", names(myTidyDF))
+names(myTidyDF) <- gsub("tGravityAcc", "gravityAcclTime", names(myTidyDF))
+names(myTidyDF) <- gsub("fBodyAcc", "bodyAcclFrequency", names(myTidyDF))
+names(myTidyDF) <- gsub("fGravityAcc", "gravityAcclFrequency", names(myTidyDF))
+names(myTidyDF) <- gsub("fBodyGyro", "bodyGyroFrequency", names(myTidyDF))
+names(myTidyDF) <- gsub("fBodyBodyGyroMagMean", "bodyGyroFrequencyMagnitude", names(myTidyDF))
+names(myTidyDF) <- gsub("fBodyBodyGyroJerkMag", "bodyGyroJerkFrequencyMagnitude", names(myTidyDF))
+names(myTidyDF) <- gsub("fBodyBodyAccJerkMag", "bodyAcclJerkFrequencyMagnitude", names(myTidyDF))
+names(myTidyDF) <- gsub("MagMean", "MagnitudeMean", names(myTidyDF))
+names(myTidyDF) <- gsub("MagStandardDeviation", "MagnitudeStandardDeviation", names(myTidyDF))
+names(myTidyDF) <- gsub("[-]X", "OnXAxis", names(myTidyDF))
+names(myTidyDF) <- gsub("[-]Y", "OnYAxis", names(myTidyDF))
+names(myTidyDF) <- gsub("[-]Z", "OnZAxis", names(myTidyDF))
 
 ################# Write the tidy data to a file #################
-write.table(x=myTidyDF, file="Week3_Project_TidyData.txt", sep="\t",row.names = TRUE, col.names = TRUE,)
+write.table(x=myTidyDF, file="Week3_Project_TabDelimited_TidyData.txt", sep="\t",row.names = TRUE, col.names = TRUE,)
